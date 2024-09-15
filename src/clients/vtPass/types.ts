afce6562f00ed1e5d8efaea8615f5127ae5baa2a
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-15 15:04:52 +0100

    Refactor VTpass types to include showmax voucher and optional fields in SmartCardVerifyResponse

--- src/clients/vtPass/types.ts
+++ src/clients/vtPass/types.ts
@@ -147,6 +147,8 @@ export interface VTpassPurchaseResponse extends BaseResponse {
     content?: {
         transactions: TransactionDetails;
     };
+    // showmax voucher
+    Voucher?: string;
 }
 
 // Interface for the product options response from VTpass API
@@ -179,10 +181,11 @@ export interface SmartCardVerifyResponse extends BaseResponse {
         Customer_Name: string;
         Status: string;
         DUE_DATE: string;
-        Customer_Number: number;
-        Customer_Type: string;
-        Current_Bouquet: string;
-        Current_Bouquet_Code: string;
-        Renewal_Amount: number;
+        Customer_Number?: number;
+        Customer_Type?: string;
+        Current_Bouquet?: string;
+        Current_Bouquet_Code?: string;
+        Renewal_Amount?: number;
+        Customer_ID?: number;
     };
 }
