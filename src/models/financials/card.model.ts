1c0b3a5d61c8b9b454c9c98dd3aa2ec859e47545
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-20 01:55:18 +0100

    Refactor Card model to include validation for account number length and limit the number of cards per user

--- src/models/financials/card.model.ts
+++ src/models/financials/card.model.ts
@@ -1,10 +1,11 @@
 import {
-    Table, Column, Model, DataType, ForeignKey, BelongsTo, HasMany,
+    Table, Column, Model, DataType, ForeignKey, BelongsTo, BeforeCreate, BeforeUpdate,
 } from 'sequelize-typescript';
 import User from '../user.model';
+import { BadRequestError } from 'utils/customErrors';
 
 @Table
-export class Card extends Model<Card | ICard> {
+export default class Card extends Model<Card | ICard> {
     @Column({
         type: DataType.UUID,
         defaultValue: DataType.UUIDV4,
@@ -23,29 +24,40 @@ export class Card extends Model<Card | ICard> {
         type: DataType.STRING,
         allowNull: false,
     })
-        authorization_code: string;
+        authorizationCode: string;
 
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
-        card_type: string;
+        cardType: string;
 
     @Column({
         type: DataType.STRING,
         allowNull: false,
+        validate: {
+            len: [4, 4],
+        },
     })
         last4: string;
 
     @Column({
         type: DataType.STRING,
         allowNull: false,
+        validate: {
+            isNumeric: true,
+            len: [1, 2],
+        },
     })
-        exp_month: string;
+        expMonth: string;
 
     @Column({
         type: DataType.STRING,
         allowNull: false,
+        validate: {
+            isNumeric: true,
+            len: [4, 4],
+        },
     })
         exp_year: string;
 
@@ -78,27 +90,36 @@ export class Card extends Model<Card | ICard> {
         type: DataType.STRING,
         allowNull: true,
     })
-        country_code: string;
+        countryCode: string;
 
     @Column({
         type: DataType.STRING,
         allowNull: false,
     })
         brand: string;
+
+    @BeforeCreate
+    @BeforeUpdate
+    static async checkLimit(instance: Card) {
+        const count = await Card.count({ where: { userId: instance.userId } });
+        if (count >= 3) {
+            throw new BadRequestError('User can have a maximum of 3 cards');
+        }
+    }
 }
 
 export interface ICard {
     id?: string;
     userId: string;
-    authorization_code: string;
+    authorizationCode: string;
     card_type: string;
     last4: string;
-    exp_month: string;
-    exp_year: string;
+    expMonth: string;
+    expYear: string;
     bin: string;
     bank: string;
     signature: string;
     reusable: boolean;
-    country_code?: string;
+    countryCode?: string;
     brand: string;
 }
