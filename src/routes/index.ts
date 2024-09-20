937c7bce481057dea532f63d8d909d6e58ac6753
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-21 00:46:25 +0100

    Add routes for wallet, card, bank account, and transaction CRUD operations

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,20 @@ import { Router } from 'express';
 import authRoute from './auth.routes';
 import userRoute from './user.routes';
 // import AdminRoutes from './Admin/admin.routes';
-
+import walletRoutes from './wallet.routes';
+import cardRoutes from './card.routes';
+import bankAccountRoutes from './bankAccount.routes';
+import transactionRoutes from './transaction.routes';
 
 const router = Router();
 
 router
     .use('/auth', authRoute)
     // .use('/iamPlugX', adminRoute)
+    .use('/wallet', walletRoutes)
+    .use('/card', cardRoutes)
+    .use('/bankAccount', bankAccountRoutes)
+    .use('/transaction', transactionRoutes)
     .use('/user', userRoute);
 
 export default router;
