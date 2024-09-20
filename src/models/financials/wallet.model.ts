78a73ca37f91535c62b68f188eb625a25b47a52e
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-09-20 01:55:30 +0100

    Refactor Wallet model to include balance validation and add transaction association

--- src/models/financials/wallet.model.ts
+++ src/models/financials/wallet.model.ts
@@ -2,9 +2,10 @@ import {
     Table, Column, Model, DataType, ForeignKey, BelongsTo, HasMany,
 } from 'sequelize-typescript';
 import User from '../user.model';
+import Transaction from 'models/transaction.model';
 
-@Table
-export class Wallet extends Model<Wallet | IWallet> {
+Table;
+export default class Wallet extends Model<Wallet | IWallet> {
     @Column({
         type: DataType.UUID,
         defaultValue: DataType.UUIDV4,
@@ -23,6 +24,9 @@ export class Wallet extends Model<Wallet | IWallet> {
         type: DataType.DECIMAL(10, 2),
         allowNull: false,
         defaultValue: 0,
+        validate: {
+            min: 0,
+        },
     })
         balance: number;
 
@@ -33,8 +37,9 @@ export class Wallet extends Model<Wallet | IWallet> {
     })
         currency: string;
 
+    @HasMany(() => Transaction)
+        transactions: Transaction[];
 }
-
 
 export interface IWallet {
     id?: string;
